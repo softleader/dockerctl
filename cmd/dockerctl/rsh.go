26a51ddc67a08b01d77b2988b3c7575e49111bf2
matt <methodho@gmail.com>
2019-08-27 12:22:33 +0800

    update

--- cmd/dockerctl/rsh.go
+++ cmd/dockerctl/rsh.go
@@ -10,12 +10,13 @@ import (
 
 const rshDesc = `Open a remote shell session to a container inside swarm service
 
-	$ dockerctl rsh SERVICE_ID -- COMMAND ARGS...
+	$ dockerctl rsh SERVICE_ID COMMAND -- ARGS...
 
 Example:
 
-	$ dockerctl rsh SERVICE_ID -- bash
-	$ dockerctl rsh SERVICE_ID -- curl localhost:8080/actuator/health
+	$ dockerctl rsh SERVICE_ID bash
+	$ dockerctl rsh SERVICE_ID top
+	$ dockerctl rsh SERVICE_ID ps -- -aux
 `
 
 type rshCmd struct {
@@ -30,13 +31,10 @@ func newRshCmd() *cobra.Command {
 		Use:   "rsh",
 		Short: "Start a shell session in a container",
 		Long:  rshDesc,
-		Args:  cobra.MinimumNArgs(1),
+		Args:  cobra.MinimumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			c.service = args[0]
 			c.args = args[1:]
-			if len(c.args) == 0 {
-				return fmt.Errorf(`requires at least 1 arg(s) after double-dash "--", only received 0`)
-			}
 			return c.run()
 		},
 	}
